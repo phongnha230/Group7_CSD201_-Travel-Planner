38a0c7d65f70029659e8b990098ebaed63551834
Phong Nha dev <trandophuongnhatl3@gmail.com>
2026-01-15 09:00:43 +0700

    add properties to TourLocation class for id, name, description, and price

--- src/main/java/com/travelplanner/entities/TourLocation.java
+++ src/main/java/com/travelplanner/entities/TourLocation.java
@@ -1,5 +1,8 @@
 package com.travelplanner.entities;
 
 public class TourLocation {
-    
+    private String id;
+    private String name;
+    private String description;
+    private double price;
 }

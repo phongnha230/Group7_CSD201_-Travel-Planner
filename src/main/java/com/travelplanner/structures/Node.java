8f64139fae843806bd1f7c101cd9f7b036530495
Phong Nha dev <trandophuongnhatl3@gmail.com>
2026-01-15 13:11:50 +0700

    format Node class constructor for improved readability

--- src/main/java/com/travelplanner/structures/Node.java
+++ src/main/java/com/travelplanner/structures/Node.java
@@ -3,6 +3,10 @@ package com.travelplanner.structures;
 public class Node<T>{
     public T data;
     public Node<T> next;
-
+     
+    public Node(T data) {
+        this.data = data;
+        this.next = null;
+    }
     
 }

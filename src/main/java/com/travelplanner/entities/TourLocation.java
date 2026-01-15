5deecb16db30a31cd3dfb9a80525bb647e781f5f
Linh <khanhlinhz2k4@gmail.com>
2026-01-15 13:11:26 +0700

    feat: Implement initial Travel Planner project structure, core entities, data structures, utilities, and web/console application components.

--- src/main/java/com/travelplanner/entities/TourLocation.java
+++ src/main/java/com/travelplanner/entities/TourLocation.java
@@ -5,4 +5,60 @@ public class TourLocation {
     private String name;
     private String description;
     private double price;
+
+    public TourLocation(String id, String name, String description, double price) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.price = price;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "TourLocation [id=" + id + ", name=" + name + ", description=" + description + ", price=" + price
+                + "]";
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (this == null || getClass() != obj.getClass())
+            return false;
+        TourLocation that = (TourLocation) obj;
+        return id.equals(that.id);
+    }
+
 }
